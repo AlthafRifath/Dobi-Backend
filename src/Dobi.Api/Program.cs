4757f743fadb8782cf686e09b81efa57b36598a4
AlthafRifath <althafrifath@gmail.com>
2026-07-19 23:24:11 +0530

    feat: Dockerized the application

--- src/Dobi.Api/Program.cs
+++ src/Dobi.Api/Program.cs
@@ -136,7 +136,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("DobiFrontend");
 
